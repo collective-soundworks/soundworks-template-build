856691ac846f4aa736b2c70523c36f7066486092
b-ma <benjamin.matuszewski@gmail.com>
2021-01-28 12:18:40 +0100

    reviewed watch-process, quit process with SIGINT

--- src/watch-process.js
+++ src/watch-process.js
@@ -10,7 +10,7 @@ const debounce = require('lodash.debounce');
 const processes = new Map();
 
 // run the in a forked process
-const start = function(src, inspect) {
+const start = async function(src, inspect) {
   if (!fs.existsSync(src)) {
     console.log(chalk.red(`
 Cannot start process: file "${src}" does not exists.
@@ -19,20 +19,21 @@ Cannot start process: file "${src}" does not exists.
     return;
   }
 
-  fs.stat(src, (err, stats) => {
+  fs.stat(src, async (err, stats) => {
     if (err) {
-      reject(src);
+      console.log(err);
+      return;
     }
 
     if (processes.has(src)) {
-      stop(src);
+      await stop(src);
     }
 
     const options = inspect ? { execArgv: ['--inspect'] } : {};
     const delay = inspect ? 100 : 0;
 
-    // @important - the timeout only works because of the debounce
-    // this is needed when restarting for the
+    // @important - the timeout is needed for the inspect to properly exit
+    // the value has been chosen by "rule of thumb"
     setTimeout(() => {
       const proc = fork(src, [], options);
       processes.set(src, proc);
@@ -41,16 +42,23 @@ Cannot start process: file "${src}" does not exists.
 }
 
 // kill the forked process hosting the proc
-const stop = function(src) {
-  const proc = processes.get(src);
-  let stopped = false;
+const stop = async function(src) {
+  return new Promise((resolve, reject) => {
+    const proc = processes.get(src);
 
-  if (proc) {
-    terminate(proc.pid);
-    stopped = true;
-  }
+    if (proc) {
+      terminate(proc.pid, 'SIGINT', (err) => {
+        if (err) {
+          console.log(`[@soundworks/template-build] could not stop ${src}`);
+          console.log(err);
+          reject();
+        }
+      });
+    }
 
-  processes.delete(src);
+    processes.delete(src);
+    resolve();
+  });
 }
 
 module.exports = function watchProcess(processName, inspect) {
